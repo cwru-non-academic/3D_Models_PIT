047b45e35c3e85ac05adc0bac5db21f573eb5fa1
lemfn64 <lemfn64@gmail.com>
2023-07-19 15:03:03 -0400

    fixed manipualtion
    fixed manipulation using local position and a centering object

--- Assets/Scripts/manipulations.cs
+++ Assets/Scripts/manipulations.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class manipulations : MonoBehaviour
@@ -10,17 +11,18 @@ public class manipulations : MonoBehaviour
     public float xRotation { get; set; }
     public float yRotation { get; set; }
     public float zRotation { get; set; }
+    private Vector3 cameraOriginalPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraOriginalPosition = cameraView.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        cameraView.transform.position = new Vector3(cameraView.transform.position.x, scale, cameraView.transform.position.z);
-        //cameraHolder.transform.localEulerAngles = new Vector3(xRotation, yRotation, zRotation);
+        cameraView.transform.localPosition = new Vector3(cameraOriginalPosition.x, cameraOriginalPosition.y-scale , cameraOriginalPosition.z);
+        cameraHolder.transform.localEulerAngles = new Vector3(xRotation, yRotation, zRotation);
     }
 
     
